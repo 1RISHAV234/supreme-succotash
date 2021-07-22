adc36b26924e1219e424b0980f96fc74ec13980b
1RISHAV234 <83695942+1RISHAV234@users.noreply.github.com>
2021-07-22 18:26:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var name_first = "Rishav";
+var second_name = "Bhakat";
+var concat = name_first.concat(second_name);
+console.log("My first console project")
+console.log(concat);
+var sqrt = Math.sqrt(25);
+console.log("Square root of 25 is",sqrt);
+var greater = Math.max(6 , 10);
+console.log("Who is greater? 6 or 10?",greater);
+var lc = "variables data can be changed anytime :)"
+var cap = lc.toUpperCase();
+console.log("Original text - ",lc);
+console.log("Uppercase text",cap);
+var low = cap.toLowerCase();
+console.log("Same capital letter to uppercase letter:",low);
+var rep = "Numbers are also known as text"
+var repcha = rep.replace("Numbers","Strings");
+console.log("Wrong text -",rep);
+console.log("Correct text -",repcha)
+var pow = Math.pow(5 , 2);
+console.log("The power of 5² is -",pow)
